59e02684edaf04b97ff3eaa7954c356a332d5c35
RaxEmRemy <pwnagepal@gmail.com>
2017-06-11 03:58:15 -0400

    Functioning editor now prints out ASCII numbers of each entered character.

--- kilo.c
+++ kilo.c
@@ -1,3 +1,5 @@
+#include <ctype.h>
+#include <stdio.h>
 #include <unistd.h>
 #include <termios.h>
 #include <stdlib.h>
@@ -9,11 +11,11 @@ void disableRawMode() {
 }
 
 void enableRawMode() {
-  tcgetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+  tcgetattr(STDIN_FILENO, &orig_termios);
   atexit(disableRawMode);
 
   struct termios raw = orig_termios;
-  raw.c_lflag &= ~(ECHO);
+  raw.c_lflag &= ~(ECHO | ICANON);
 
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
@@ -21,6 +23,13 @@ void enableRawMode() {
 int main() {
   enableRawMode();
   char c;
-  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+  if (iscntrl(c)) {
+    printf("%d\n", c);
+  }
+  else {
+    printf("%d ('%c')\n", c, c);
+  }
+}
   return 0;
 }
